fbd6ab682881895c9ab4ce805d78188337c077c6
Ultimatetyson3 <Danpor490000@gmail.com>
2024-11-16 12:10:28 -0500

    I added a Main Menu
    I made a new scene that is our main menu

---
+++ WolfJam/Assets/Scripts/Main Menu/MainMenuScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour 
+{
+    
+
+    public void Play()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+        Debug.Log("User has quit the game");
+    }
+
+}
