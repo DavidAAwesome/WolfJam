c59726730f314db4545691336ce308ddec3aa726
WilsonXia <wx4477@rit.edu>
2024-11-16 13:36:41 -0500

    players created

---
+++ WolfJam/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public enum PlayerType
+{
+    PlayerOne,
+    PlayerTwo
+}
+public class Player : MonoBehaviour
+{
+    public PlayerType playerType;
+    PlayerController controller;
+    public bool usePolarity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        SetUpControls();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        usePolarity = controller.useAction;
+        if (usePolarity) {
+            Debug.Log("Is on.");
+        }
+    }
+
+    void SetUpControls()
+    {
+        switch (playerType)
+        {
+            case PlayerType.PlayerOne:
+                controller.up = KeyCode.W;
+                controller.down = KeyCode.S;
+                controller.left = KeyCode.A;
+                controller.right = KeyCode.D;
+                controller.action = KeyCode.LeftShift;
+                break;
+            case PlayerType.PlayerTwo:
+                controller.up = KeyCode.UpArrow;
+                controller.down = KeyCode.DownArrow;
+                controller.left = KeyCode.LeftArrow;
+                controller.right = KeyCode.RightArrow;
+                controller.action = KeyCode.RightShift;
+                break;
+        }
+    }
+}
