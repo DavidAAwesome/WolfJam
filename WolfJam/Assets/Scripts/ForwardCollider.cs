0b1953e17166372f116e2069847b515f32bc45fd
WilsonXia <wx4477@rit.edu>
2024-11-16 18:20:49 -0500

    barrier obstacles created

--- WolfJam/Assets/Scripts/ForwardCollider.cs
+++ WolfJam/Assets/Scripts/ForwardCollider.cs
@@ -2,13 +2,34 @@ using UnityEngine;
 
 public class ForwardCollider : MonoBehaviour
 {
+    Player player;
     public bool isColliding = false;
+    private void Start()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Wall") 
         {
             isColliding = true;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            if(collision.gameObject.GetComponent<Barrier>().charge == player.charge)
+            {
+                isColliding = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isColliding = false;
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
--- WolfJam/Assets/Scripts/Obstacles/Barrier.cs
+++ WolfJam/Assets/Scripts/Obstacles/Barrier.cs
@@ -9,19 +9,4 @@ public class Barrier : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player") 
-        {
-            // Get the player's charge
-            // If playerCharge matches with the barrier's charge
-            Player player = collision.gameObject.GetComponent<Player>();
-            if (player.charge == charge) 
-            {
-                // Prevent the player from going through
-                Debug.Log("Blocking player...");
-            }
-        }
-    }
 }
