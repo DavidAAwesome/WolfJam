e11fc82d6a6afb43c6415bd038044cde3d2f351d
David Aduku <130676739+DavidAAwesome@users.noreply.github.com>
2024-11-16 00:53:40 -0500

    Basic Movement

---
+++ WolfJam/Assets/Scripts/PlusController.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class PlusController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public Rigidbody2D rb;
+
+    private bool isGrounded = false;
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * (speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * (speed * Time.deltaTime);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)){
+            if(isGrounded){
+                rb.AddForce(Vector3.up * jumpForce);
+                isGrounded = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.CompareTag("Ground"))
+            isGrounded = true;
+    }
+    
+}
