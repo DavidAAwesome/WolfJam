6b0c1be740f67c87c3e8f38d12dfa0a9e81780bb
WilsonXia <wx4477@rit.edu>
2024-11-16 02:55:14 -0500

    finished making action button

--- WolfJam/Assets/Scripts/Player/PlayerOne.cs
+++ WolfJam/Assets/Scripts/Player/PlayerOne.cs
@@ -3,19 +3,31 @@ using UnityEngine;
 public class PlayerOne : MonoBehaviour
 {
     PlayerController controller;
+
+    public bool usePolarity;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<PlayerController>();
-        controller.up = KeyCode.W;
-        controller.down = KeyCode.S;
-        controller.left = KeyCode.A;
-        controller.right = KeyCode.D;
+        SetUpControls();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        usePolarity = controller.useAction;
+        if (usePolarity) {
+            Debug.Log("Is on.");
+        }
+    }
+
+    void SetUpControls()
+    {
+        controller.up = KeyCode.W;
+        controller.down = KeyCode.S;
+        controller.left = KeyCode.A;
+        controller.right = KeyCode.D;
+        controller.action = KeyCode.LeftShift;
     }
 }
